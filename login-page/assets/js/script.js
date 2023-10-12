11dae46856c640cc51beb2b5dbb8713f4678f4ea
Wutdehel <elizanlilongongopan@gmail.com>
2023-10-12 21:12:31 +0700

    feat: Adding login and regist file

---
+++ login-page/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#toggle-password").click(function() {
+        var passwordField = $("#password");
+        var passwordFieldType = passwordField.attr('type');
+
+        if (passwordFieldType === 'password') {
+            passwordField.attr('type', 'text');
+            $("#toggle-icon").removeClass('fa-eye').addClass('fa-eye-slash');
+        } else {
+            passwordField.attr('type', 'password');
+            $("#toggle-icon").removeClass('fa-eye-slash').addClass('fa-eye');
+        }
+    });
+});
