09ab8fc78f35df5f36475611fdc9d45f582857bd
Helvizar <helvizaranderson@gmail.com>
2023-10-15 14:43:43 +0700

    feat: added money management article list

--- education-page/assets/js/script.js
+++ education-page/assets/js/script.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-  });
-  calendar.render();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var calendarEl = document.getElementById("calendar");
+//   var calendar = new FullCalendar.Calendar(calendarEl, {
+//     initialView: "dayGridMonth",
+//   });
+//   calendar.render();
+// });
+
+const dataApi = async () => {
+  const api = await fetch("https://api-berita-indonesia.vercel.app/cnn/ekonomi");
+  const data = await api.json();
+  const firstPoint = data.title
+  // items(data);
+  console.log("datanya adalah: ", data.data.posts);
+}
+
+// const items = (articles) => {
+//   articles.forEach((arcile) => {
+//     console.log(arcile)
+//   })
+// }
+
+dataApi();
